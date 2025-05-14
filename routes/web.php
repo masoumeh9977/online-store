cfc9403c37c6c14ba398078ee823afab947a361c
masoume9877 <emasoomeh50@yahoo.com>
2025-05-14 20:29:50 +0330

    feat(adding orders route)

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,8 @@
 use App\Http\Controllers\Website\IndexController;
 use Illuminate\Support\Facades\Route;
 
-Route::as('website')->prefix('')->group(function (){
+Route::as('website')->prefix('')->group(function () {
     Route::get('/', [IndexController::class, 'index']);
+    Route::get('orders', [IndexController::class, 'orders']);
 
 });
